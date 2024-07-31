ed91cd602c7bf4276bb43beb5977e1deabe9a23e
MariaMendes11 <mcmendes900@gmail.com>
2024-07-31 12:46:51 -0300

    atividades31/07

---
+++ atividade3.js
@@ -0,0 +1,10 @@
+const nota = 7
+if(nota >= 6){
+    console.log('você esta aprovado')
+}
+if(nota < 6 && nota>= 4){
+    console.log("voce esta de recuperação")
+}
+if(nota < 4){
+    console.log("voce esta reprovado")
+}
